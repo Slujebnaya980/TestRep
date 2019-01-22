3d08977153d4043e4b37bcd258a857c6a004e910
АРТ <psluzhebnaya@inbox.ru>
2019-01-22 21:46:02 +0300

    5

--- N3.java
+++ N3.java
@@ -1,3 +1,6 @@
 public class N3 {
-    public static void main(String[]args){System.out.println("N3");}
+    public static void main(String[]args){
+        System.out.println("N3");
+        System.out.println("55");
+    }
 }
